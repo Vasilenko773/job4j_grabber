12e84aa2e7f88b2a9e2174ada69019475e5ccae7
Нина <vasilenko.lesnik2017@yandex.ru>
2021-09-16 21:34:23 +0900

    1. Quartz [#175122 #203409]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -17,65 +17,34 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
-    public static void main(String[] args) {
-        try {
-            List<Long> store = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+
+        Properties ps = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            ps.load(in);
+
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDataMap data = new JobDataMap();
-            data.put("store", store);
-            JobDetail job = newJob(Rabbit.class)
-                    .usingJobData(data)
-                    .build();
+            JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(7)
+                    .withIntervalInSeconds(Integer.parseInt(ps.getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-            Thread.sleep(10000);
-            scheduler.shutdown();
-            System.out.println(store);
         } catch (SchedulerException se) {
             se.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
+
     public static class Rabbit implements Job {
-
-        public Rabbit() {
-            try (PreparedStatement statement = getConnection().prepareStatement("insert into rabbit(create_date) values (?)")) {
-                statement.setString(1, "10.01.2022");
-                statement.execute();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
         @Override
-        public void execute(JobExecutionContext context) {
-                try {
-                List<Connection> store = (List<Connection>) context.getJobDetail().getJobDataMap().get("store");
-                store.add(Rabbit.getConnection());
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        private static Connection getConnection() throws Exception {
-            Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://127.0.0.1:5432/schema";
-            String login = "postgres";
-            String password = "password";
-            return DriverManager.getConnection(url, login, password);
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
         }
     }
 }
-
-
