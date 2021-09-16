e240e61e96b4d1cf16259dda0260264468f66e71
Нина <vasilenko.lesnik2017@yandex.ru>
2021-09-16 15:56:08 +0900

    2. Парсинг HTML страницы.

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,20 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".altCol");
+        for (Element td : row) {
+        Element parent = td.parent();
+        Element children = parent.child(5);
+            System.out.println(children.text());
+            break;
+        }
+        }
+    }
+
