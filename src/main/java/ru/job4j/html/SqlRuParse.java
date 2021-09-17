c21071e54992c848efaef5a0ff24f31dc89cc948
Нина <vasilenko.lesnik2017@yandex.ru>
2021-09-17 20:56:30 +0900

    2. Парсинг HTML страницы. [#260358 #203856]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -8,15 +8,10 @@ import org.jsoup.select.Elements;
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
+        Elements row = doc.select("td[style].altCol");
         for (Element td : row) {
-            Element href = td.child(0);
-            Element parent = td.parent();
-            Element children = parent.child(5);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(children.text());
-        }
+            System.out.println(td.text());
         }
     }
+}
 
