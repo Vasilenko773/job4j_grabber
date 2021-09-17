0041afe3f3d484aa8148bf4e1347fbfda84b12f2
Нина <vasilenko.lesnik2017@yandex.ru>
2021-09-17 23:43:37 +0900

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,68 @@
+package ru.job4j.grabber.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
+import java.nio.charset.StandardCharsets;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+
+    private static Map<String, String> MONTHS = Map.ofEntries(
+            Map.entry("янв", "1"),
+            Map.entry("фев", "2"),
+            Map.entry("мар", "3"),
+            Map.entry("апр", "4"),
+            Map.entry("май", "5"),
+            Map.entry("июн", "6"),
+            Map.entry("июл", "7"),
+            Map.entry("авг", "8"),
+            Map.entry("сен", "9"),
+            Map.entry("окт", "10"),
+            Map.entry("ноя", "11"),
+            Map.entry("дек", "12")
+    );
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        String[] wholeDate = parse.split(", ");
+        String[] date = wholeDate[0].split(" ");
+        LocalDate lD = null;
+
+        if (date.length == 3) {
+            lD = LocalDate.parse(String.format("%s %s %s", date[0], MONTHS.get(date[1]), date[2]), DateTimeFormatter.ofPattern("d M yy"));
+
+        } else if (date[0].contains("сегодня")) {
+            lD = LocalDate.now();
+
+        } else {
+            lD = LocalDate.now().minusDays(1);
+        }
+        LocalTime lT = LocalTime.parse(wholeDate[1], DateTimeFormatter.ofPattern("HH:mm"));
+
+        return LocalDateTime.of(lD, lT);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select("td[style].altCol");
+        for (Element td : row) {
+
+            LocalDateTime exp = sqlRuDateTimeParser.parse(td.text());
+            System.out.println(exp);
+        }
+    }
+}
