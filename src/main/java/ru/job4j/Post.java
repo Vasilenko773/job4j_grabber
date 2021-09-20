d61f9ba2cec880452783351cb1c4a606e85188c3
Нина <vasilenko.lesnik2017@yandex.ru>
2021-09-20 17:36:18 +0900

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/Post.java
+++ src/main/java/ru/job4j/Post.java
@@ -1,7 +1,14 @@
 package ru.job4j;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.sql.Statement;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Post {
@@ -26,7 +33,7 @@ public class Post {
         }
         Post post = (Post) o;
         return id == post.id && Objects.equals(title, post.title) && Objects.equals(link, post.link)
-        && Objects.equals(created, post.created);
+                && Objects.equals(created, post.created);
     }
 
     @Override
@@ -38,10 +45,35 @@ public class Post {
     public String toString() {
         return "Post{"
                 + "id=" + id
-               + ", title='" + title + '\''
-               + ", link='" + link + '\''
-               + ", description='" + description + '\''
-               + ", created=" + created
-               + '}';
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+
+    public static List<String> loadData(String url) throws Exception {
+        Document doc = Jsoup.connect(url).get();
+        Elements row = doc.select(".msgTable");
+        List<String> rsl = new ArrayList();
+
+        for (Element td : row) {
+            Element message = td.children().get(0).children().get(1).children().get(1);
+            Element date = td.children().get(0).children().get(2).children().get(0);
+            String[] dateArray = date.text().split(", ");
+            String[] dateArray2 = dateArray[1].split(" ");
+
+            String day = dateArray[0] + ", " + dateArray2[0];
+
+            rsl.add("СООБЩЕНИЕ: " + message + "ДАТА: " + day);
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<String> exp = loadData("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
+        for (String s : exp) {
+            System.out.println(s);
+        }
     }
 }
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,10 +7,16 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select("td[style].altCol");
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t").get();
+        Elements row = doc.select(".msgTable");
+
         for (Element td : row) {
-            System.out.println(td.text());
+            Element message = td.children().get(0).children().get(1).children().get(1);
+            Element date = td.children().get(0).children().get(2).children().get(0);
+            String[] dateArray = date.text().split(", ");
+            String[] dateArray2 = dateArray[1].split(" ");
+            String day = dateArray[0] + ", " + dateArray2[0];
+            System.out.println("СООБЩЕНИЕ: " + message.text() + "ДАТА: " + day);
         }
     }
 }
